7e402a7981607cb04f8a52e0e0d875ac816641bb
ajaymittal696 <ajaymittal696@gmail.com>
2025-06-19 16:56:16 +0530

    updated

--- header.php
+++ header.php
@@ -44,7 +44,7 @@
 						<li><a class="dropdown-item" href="./products">Loom Products</a></li>
 					</ul>
 				</li>
-				<li class="nav-item "> <a class="nav-link" href="./story">Stories </a> </li>
+				<li class="nav-item "> <a class="nav-link" href="./story">Our Story </a> </li>
 				<li class="nav-item "> <a class="nav-link" href="./infrastructure">Infrastructure </a> </li>
 				<li class="nav-item "> <a class="nav-link" href="./about">About Us </a> </li>
 				<li class="nav-item"> <a class="nav-link" href="./contact">Contact Us </a> </li>
